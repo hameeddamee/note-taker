8a499337364fbdeee5fd41b975a94d05d190498d
Hameed Damee <dameeimpact@gmail.com>
2019-10-28 10:41:15 +0100

    express validator setup

--- server.js
+++ server.js
@@ -46,6 +46,26 @@ app.use(function(req, res, next) {
   next();
 });
 
+// Express Validator Middleware
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
 // Passport Config
 require("./config/passport")(passport);
 // Passport Middleware
